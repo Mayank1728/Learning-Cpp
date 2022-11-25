3c532ec043ec2c99fbeee1ec821eebec879cb6f0
Mayank Mudgal <mayankmudgal111+github@gmail.com>
2022-11-25 14:21:50 +0530

    Learning cpp

---
+++ Intro to cpp/styleGuide.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ cout << "Hello World!";
+ return 0;
+}
+// This is a single line comment
+/*
+   This is a multi line comment
+   comments and whitespaces are ignored.
+*/
+/*
+  Every c++ program starts with a main()
+  it always returns an error code
+  0 means Everything's fine
+  1-255 error codes have special value
+
+*/
